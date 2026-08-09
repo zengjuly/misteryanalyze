57f47e618dca1e53623d85e24e9583f70ae3c7c3
Mystery Team <mystery-team@example.com>
2026-08-10 02:44:47 +0800

    daily shell

--- daily.sh
+++ daily.sh
@@ -1,7 +1,18 @@
 #/bin/bash
 #
+
 source /home/ai/ai_runner/venv/bin/activate
 
+git pull
+
 python3 run_analysis.py --mode daily
+
+cd /home/ai/ai_runner/stock/output
+
+git add .
+
+git commit -m "daily"
+
+git push
 
 
